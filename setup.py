dca1d8ac8dde726f465e8cfb5b1040d33b6bb60b
amitranjasahoo <amitranjasahoo@gmail.com>
2022-08-03 16:00:29 +0530

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It is bank credit risk using mloops',
+    author='AMIT RANJAN SAHOO',
+    license='MIT',
+)
